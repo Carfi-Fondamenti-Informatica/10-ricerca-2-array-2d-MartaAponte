5ab7069735fbd8a522a185042197e5b40c1bba88
MartaAponte <128090552+MartaAponte@users.noreply.github.com>
2023-06-08 22:58:47 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,61 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char lista[10][20];
+    char a[20];
+    int pos = 0;
+
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++) {
+            a[j] = ' ';
+            lista[i][j] = ' ';
+        }
+    }
+
+    for(int i=0;i<10;i++){
+        cin >> lista[i];
+    }
+
+    cin >> a;
+    if(cerca(lista,pos,a)){
+        cout << "trovato in posizione "<< pos;
+    }else{
+        cout << "non trovato";
+    }
+
+    return 0;
 }
+
+
+
+//cpp
+
+
+#include "lib.h"
+
+bool cerca(char listanome[10][20], int &pos, char a[]){
+    for(int i=0;i<10;i++){
+
+        for(int j=0;j<20;j++){
+            if(listanome[i][j] != a[j]){
+                  j = 20;
+            }else{
+
+                    if(j==19){
+                          pos = (i)/2;
+
+                           return true;
+
+                    }}}
+    }
+    return false;
+}
+
+
+
+
+//h
+
+bool cerca(char[10][20], int &, char[])
